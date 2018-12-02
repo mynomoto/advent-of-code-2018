f9e33cc03b32b52bca763a6545c877bf2a9e3e78
mynomoto <mynomoto@gmail.com>
2018-12-02 14:51:39 -0200

    Refactor

--- src/advent_of_code_2018/core.clj
+++ src/advent_of_code_2018/core.clj
@@ -8,40 +8,45 @@
   [& args]
   (println "Hello, World!"))
 
-(defn day1-part1
-  []
-  (-> (io/resource "day01-input")
+(defn read-line-input
+  [filename]
+  (-> (io/resource filename)
       slurp
-      str/split-lines
-      (->>
-        (map #(Integer/parseInt %))
-        (apply +))))
+      str/split-lines))
+
+(def day-1-input (read-line-input "day01-input"))
+
+(defn day1-part1
+  [day-1-input]
+  (->> day-1-input
+       (map #(Integer/parseInt %))
+       (apply +)))
 
 (defn day1-part2
-  []
-  (-> (io/resource "day01-input")
-      slurp
-      str/split-lines
-      (->>
-        (map #(Integer/parseInt %))
-        cycle
-        (reduce (fn [{:keys [:frequency :seen]} i]
-                  (let [new-frequency (+ frequency i)]
-                    (if (contains? seen new-frequency)
-                      (reduced new-frequency)
-                      {:frequency new-frequency
-                       :seen (conj seen new-frequency)}))) {:frequency 0
-                     :seen #{0}}))))
+  [day-1-input]
+  (->>
+    day-1-input
+    (map #(Integer/parseInt %))
+    cycle
+    (reduce (fn [{:keys [:frequency :seen]} i]
+              (let [new-frequency (+ frequency i)]
+                (if (contains? seen new-frequency)
+                  (reduced new-frequency)
+                  {:frequency new-frequency, :seen (conj seen new-frequency)})))
+      {:frequency 0, :seen #{0}})))
+
+(def day-2-input (read-line-input "day02-input"))
 
 (defn day2-part1
-  []
-  (let [freq (-> (io/resource "day02-input")
-                 slurp
-                 str/split-lines
-                 (->> (map #(-> % frequencies vals set))))
-        two (count (filter #(% 2) freq))
-        three (count (filter #(% 3) freq))]
-    (* two three)))
+  [day-2-input]
+  (let [freq (map #(-> %
+                       frequencies
+                       vals
+                       set)
+               day-2-input)
+        twos (count (filter #(% 2) freq))
+        threes (count (filter #(% 3) freq))]
+    (* twos threes)))
 
 (defn count-differences
   [acc s1 s2]
@@ -70,14 +75,16 @@
         (recur acc (subs s1 1) (subs s2 1))))))
 
 (defn day2-part2
-  []
-  (let [words (-> (io/resource "day02-input")
-                  slurp
-                  str/split-lines
-                  sort)]
+  [day-2-input]
+  (let [words (sort day-2-input)]
     (reduce (fn [words candidate]
               (if-some [result (check-prototype? candidate words)]
                 (reduced (apply remove-difference "" result))
                 (rest words)))
       (rest words)
       words)))
+
+(comment (day1-part1 day-1-input)
+         (day1-part2 day-1-input)
+         (day2-part1 day-2-input)
+         (day2-part2 day-2-input))
