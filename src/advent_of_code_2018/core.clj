de6b5589885ec63975f10cf1d06f73cd4c135917
mynomoto <mynomoto@gmail.com>
2018-12-02 14:40:33 -0200

    Day 2

--- src/advent_of_code_2018/core.clj
+++ src/advent_of_code_2018/core.clj
@@ -33,3 +33,51 @@
                        :seen (conj seen new-frequency)}))) {:frequency 0
                      :seen #{0}}))))
 
+(defn day2-part1
+  []
+  (let [freq (-> (io/resource "day02-input")
+                 slurp
+                 str/split-lines
+                 (->> (map #(-> % frequencies vals set))))
+        two (count (filter #(% 2) freq))
+        three (count (filter #(% 3) freq))]
+    (* two three)))
+
+(defn count-differences
+  [acc s1 s2]
+  (if (str/blank? s1)
+    acc
+    (let [char (subs s1 0 1)]
+      (if (= char (subs s2 0 1))
+        (recur acc (subs s1 1) (subs s2 1))
+        (recur (inc acc) (subs s1 1) (subs s2 1))))))
+
+(defn check-prototype?
+  [candidate words]
+  (loop [[attempt & r] words]
+    (case (count-differences 0 candidate attempt)
+      1 [candidate attempt]
+      2 (recur r)
+      nil)))
+
+(defn remove-difference
+  [acc s1 s2]
+  (if (str/blank? s1)
+    acc
+    (let [char (subs s1 0 1)]
+      (if (= char (subs s2 0 1))
+        (recur (str acc char) (subs s1 1) (subs s2 1))
+        (recur acc (subs s1 1) (subs s2 1))))))
+
+(defn day2-part2
+  []
+  (let [words (-> (io/resource "day02-input")
+                  slurp
+                  str/split-lines
+                  sort)]
+    (reduce (fn [words candidate]
+              (if-some [result (check-prototype? candidate words)]
+                (reduced (apply remove-difference "" result))
+                (rest words)))
+      (rest words)
+      words)))
